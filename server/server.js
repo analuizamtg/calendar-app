79aff8ea30b87c6120659a5c69cea0f0da4573bf
analuizamtg <analuizamtg@gmail.com>
2017-07-19 15:27:49 -0300

    Adding cors dependency

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cors = require('cors')
 
 var app = express();
+app.use(cors());
 app.use(bodyParser());
 
 var routes = require('./routes');
