693f24584221dcd4a84f3127442f61bd46e615d2
analuizamtg <analuizamtg@gmail.com>
2017-07-19 16:22:56 -0300

    Fixing heroku port number

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var app = express();
 app.use(cors());
 app.use(bodyParser());
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+const PORT = process.env.PORT || 5000;
 
 var routes = require('./routes');
 app.use('/', routes.router);
@@ -15,9 +16,8 @@ app.use('/', routes.router);
 mongoose.connect("mongodb://admin:admin@ds157342.mlab.com:57342/calendar");
 
 function start () {
- 	var port = 5000;;
- 	app.listen(port);
- 	console.log('Server started on port ' + port);
+ 	app.listen(PORT);
+ 	console.log('Server started on port ${PORT}');
 }
 
 exports.app = app;
